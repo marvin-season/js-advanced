c86f57efd42492c95fb54f02a0a8789fccec80e2
marvin <sea-son@foxmail.com>
2024-11-05 14:05:42 +0800

    feat: 卖出股票I

--- apps/core/algorithm/labuladong/7.max_profit.js
+++ apps/core/algorithm/labuladong/7.max_profit.js
@@ -1,4 +1,4 @@
-const prices = [7, 6, 4, 3, 1];
+const prices = [7, 6, 1, 3, 5, 9];
 /**
  * 如果第i天卖出股票，则最大利润为(该天的股价-前面天数中最小的股价)，然后与已知的最大利润比较，如果大于则更新当前最大利润的值。厉害，学习了。
  * @param prices
@@ -6,12 +6,11 @@ const prices = [7, 6, 4, 3, 1];
  */
 const maxProfit = function(prices) {
   let profit = -Infinity;
+  let minBuy = Infinity;
 
-  for (let i = prices.length - 1; i >= 0; i--) {
-    const min = Math.min(...prices.slice(0, i));
-    if (prices[i] - min >= profit) {
-      profit = prices[i] - min;
-    }
+  for (let i = 1; i <= prices.length - 1; i++) {
+    minBuy = Math.min(minBuy, prices[i - 1]);
+    profit = Math.max(prices[i] - minBuy, profit);
   }
 
   return profit < 0 ? 0 : profit;
