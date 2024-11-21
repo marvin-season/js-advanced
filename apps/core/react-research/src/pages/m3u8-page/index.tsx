0ed1a2ff91d954430216da6ea2c89b2da2915b05
marvin <sea-son@foxmail.com>
2024-11-21 18:40:52 +0800

    👌 IMPROVE: perf

--- apps/core/react-research/src/pages/m3u8-page/index.tsx
+++ apps/core/react-research/src/pages/m3u8-page/index.tsx
@@ -2,32 +2,32 @@ import React, { useEffect, useRef } from 'react';
 import Hls from 'hls.js';
 
 const VideoPlayer = ({ src }) => {
-  const videoRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    if (videoRef.current) {
-      if (Hls.isSupported()) {
-        const hls = new Hls();
-        hls.loadSource(src);
-        hls.attachMedia(videoRef.current);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
-          videoRef.current.play();
-        });
+    if (!videoRef.current) {
+      return;
+    }
 
-        return () => {
-          hls.destroy();
-        };
-      } else if (
-        videoRef.current.canPlayType('application/vnd.apple.mpegurl')
-      ) {
-        // 对于支持原生 HLS 的浏览器（如 Safari）
-        videoRef.current.src = src;
-        videoRef.current.addEventListener('loadedmetadata', () => {
-          videoRef.current.play();
-        });
-      } else {
-        console.error('This browser does not support HLS.');
-      }
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(src);
+      hls.attachMedia(videoRef.current);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        videoRef.current?.play();
+      });
+
+      return () => {
+        hls.destroy();
+      };
+    } else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+      // 对于支持原生 HLS 的浏览器（如 Safari）
+      videoRef.current.src = src;
+      videoRef.current.addEventListener('loadedmetadata', () => {
+        videoRef.current?.play();
+      });
+    } else {
+      console.error('This browser does not support HLS.');
     }
   }, [src]);
 
