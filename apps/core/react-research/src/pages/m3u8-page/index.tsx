69ee02c47adb1a9d47dab55b9f461248a48aa83a
marvin <sea-son@foxmail.com>
2024-11-21 15:08:36 +0800

    📦 NEW: hls

---
+++ apps/core/react-research/src/pages/m3u8-page/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
+
+const VideoPlayer = ({ src }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(videoRef.current);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          videoRef.current.play();
+        });
+
+        return () => {
+          hls.destroy();
+        };
+      } else if (
+        videoRef.current.canPlayType('application/vnd.apple.mpegurl')
+      ) {
+        // 对于支持原生 HLS 的浏览器（如 Safari）
+        videoRef.current.src = src;
+        videoRef.current.addEventListener('loadedmetadata', () => {
+          videoRef.current.play();
+        });
+      } else {
+        console.error('This browser does not support HLS.');
+      }
+    }
+  }, [src]);
+
+  return <video ref={videoRef} controls style={{ width: '100%' }} />;
+};
+
+export default function M3u8Page() {
+  return (
+    <div>
+      <h2>M3u8Page</h2>
+      <VideoPlayer src="/local/m3u8/smartvision_c059ebb624.m3u8" />
+    </div>
+  );
+}
--- apps/core/react-research/vite.config.ts
+++ apps/core/react-research/vite.config.ts
@@ -30,6 +30,11 @@ export default defineConfig({
     port: 10003,
     host: true,
     proxy: {
+      '/local/': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace('/local/', '')
+      },
       '/api/': {
         target: 'http://10.0.44.34:5000', // 开发环境
         ws: true,
